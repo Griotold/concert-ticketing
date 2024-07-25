610a7d76afbfa412ba2169bdba1b6caef23b24fb
Griotold <gotjd9773@naver.com>
2024-07-25 11:07:46 +0900

    좌석 예약 동시성 테스트 3000명 - 분산락 - 범위 좁혀서 - 2초 ~ 3초

--- src/main/kotlin/org/griotold/concert/application/reservation/PerformanceSeatReservationUseCase.kt
+++ src/main/kotlin/org/griotold/concert/application/reservation/PerformanceSeatReservationUseCase.kt
@@ -4,19 +4,27 @@ import org.griotold.concert.aop.DistributedLock
 import org.griotold.concert.application.common.UseCase
 import org.griotold.concert.application.reservation.command.ReservationCommand
 import org.griotold.concert.domain.performance.PerformanceService
+import org.griotold.concert.domain.performance.Seat
 import org.griotold.concert.domain.reservation.ReservationService
+import org.springframework.transaction.annotation.Transactional
 
-//@Transactional
+@Transactional
 @UseCase
 class PerformanceSeatReservationUseCase(
     private val performanceService: PerformanceService,
     private val reservationService: ReservationService
 ) {
-    // 분산락 사용 - Redisson
-    @DistributedLock(key = "#lockName")
+
+
     operator fun invoke(command: ReservationCommand.Reserve) {
-        val seat = performanceService.getSeat(command.seatId)
+        val seat = getSeat(command.seatId)
         performanceService.reserve(seat)
         reservationService.reserve(command.userId, command.seatId, seat.price)
+    }
+
+    // 분산 락 여기에만 적용!
+    @DistributedLock(key = "#lockName")
+    private fun getSeat(seatId: Long) : Seat {
+        return performanceService.getSeat(seatId)
     }
 }
